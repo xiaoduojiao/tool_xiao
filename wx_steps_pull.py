e98421d9c8f79c0a8268d6953c2a97ee8168badb
xiaoduojiao <384526532@qq.com>
2023-06-13 17:45:56 +0800

    Initial commit

---
+++ wx_steps_pull.py
@@ -0,0 +1,27 @@
+# 同步微信步数
+
+def wxstep(steps):
+    import requests
+    s = requests.session()
+
+    url =r'http://118.195.237.33/'
+    res = s.get(url)
+
+    import re
+    csrf_token = re.findall('name="csrf_token" type="hidden" value="(.+?)"',res.text)[0]
+
+
+    url =r'http://118.195.237.33/changeXiaomiSteps'
+    data = {
+        "csrf_token": csrf_token,
+        "phone": "18770917594",
+        "password": "6382463qq",
+        "steps": steps,
+    }
+    re =s.post(url,data=data)
+    return re.json()["msg"]
+
+
+import random
+steps = random.randint(5000,6000)
+print(wxstep(steps))
